38b73eb95d3eb27b4f3b2c1372107a1676a4604a
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2022-03-30 17:46:54 +0200

    Updated version of mic data reader function, and updated mic data text files

--- Noise_calculations.py
+++ Noise_calculations.py
@@ -1,20 +1,64 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Constants
 p_ref = 20 * 10**(-6)  # Pa
 D = 0.2032             # m
 
-# Read noise data TODO
+# Read noise data
+mic1_data = np.genfromtxt('Data_txt/mic_1.txt')
+mic2_data = np.genfromtxt('Data_txt/mic_2.txt')
+mic3_data = np.genfromtxt('Data_txt/mic_3.txt')
+# mic4_data = np.genfromtxt('Data_txt/mic_4.txt')
+# mic5_data = np.genfromtxt('Data_txt/mic_5.txt')
+mic6_data = np.genfromtxt('Data_txt/mic_6.txt')
 
 
-# Calculate SPL from data TODO
-SPL = 1
+# Calculate SPL from data
+# Save to arrays
+SPL_mic1 = []
+SPL_mic2 = []
+SPL_mic3 = []
+# SPL_mic4 = []
+# SPL_mic5 = []
+SPL_mic6 = []
 
-# Correct measured SPL
-SPL_corr = 1
+# File format is each test point stored as two rows, first with f and second with SPL
+# Loop through all rows only with odd indices (SPL)
+for i in range(1, len(mic1_data)+1, 2):
+
+    SPL_mic1.append(mic1_data[i, :])
+    SPL_mic2.append(mic2_data[i, :])
+    SPL_mic3.append(mic3_data[i, :])
+    # SPL_mic4.append(mic4_data[i, :])
+    # SPL_mic5.append(mic5_data[i, :])
+    SPL_mic6.append(mic6_data[i, :])
+
+# Convert to numpy arrays
+SPL_mic1 = np.array(SPL_mic1)
+SPL_mic2 = np.array(SPL_mic2)
+SPL_mic3 = np.array(SPL_mic3)
+# SPL_mic4 = np.array(SPL_mic4)
+# SPL_mic5 = np.array(SPL_mic5)
+SPL_mic6 = np.array(SPL_mic6)
+
+# Correct measured SPL TODO
+SPL_corr_mic1 = SPL_mic1
+SPL_corr_mic2 = SPL_mic2
+SPL_corr_mic3 = SPL_mic3
+# SPL_corr_mic4 = SPL_mic4
+# SPL_corr_mic5 = SPL_mic5
+SPL_corr_mic6 = SPL_mic6
+
 
 # Calculate p_rms from corrected SPL
-p_rms = p_ref * 10 ** (SPL_corr/20)
+p_rms_mic1 = p_ref * 10 ** (SPL_corr_mic1/20)
+p_rms_mic2 = p_ref * 10 ** (SPL_corr_mic2/20)
+p_rms_mic3 = p_ref * 10 ** (SPL_corr_mic3/20)
+# p_rms_mic4 = p_ref * 10 ** (SPL_corr_mic4/20)
+# p_rms_mic5 = p_ref * 10 ** (SPL_corr_mic5/20)
+p_rms_mic6 = p_ref * 10 ** (SPL_corr_mic6/20)
+
 
 # Calculate thrust -----------------------------------
 # Read original data from text file
@@ -90,5 +134,18 @@ for i in range(len(V)):
 T = CTs * rho * n**2 * D**4  # N
 
 # Non-dimensional pressure (Pi theorem) ---------------
-Pi_p_rms = p_rms * D**2 / T
+Pi_p_rms_mic1 = p_rms_mic1 * D**2 / T
+Pi_p_rms_mic2 = p_rms_mic2 * D**2 / T
+Pi_p_rms_mic3 = p_rms_mic3 * D**2 / T
+# Pi_p_rms_mic4 = p_rms_mic4 * D**2 / T
+# Pi_p_rms_mic5 = p_rms_mic5 * D**2 / T
+Pi_p_rms_mic6 = p_rms_mic6 * D**2 / T
 
+
+# Plotting --------------------------------------------
+
+index = 0
+
+plt.plot(mic1_data[index, :], mic1_data[index+1, :])
+plt.plot(mic1_data[index, :], mic2_data[index+1, :])
+plt.show()
